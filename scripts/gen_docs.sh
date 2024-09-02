7724ec697caa446952f54bf377523b1c0e5f8a98
Jun Zhang <zhangjun990222@qq.com>
2024-09-02 15:56:36 +0800

    优化文档网页

--- scripts/gen_docs.sh
+++ scripts/gen_docs.sh
@@ -8,3 +8,5 @@ PROJECT_DIR="$(dirname "${SCRIPT_DIR}")"
 cd ${PROJECT_DIR}
 pip3 install . -vv
 pdoc -o docs/ -d markdown ./embodiedcity
+# 查找目录下的所有文件，并对每个文件执行sed命令
+find "docs/" -name "*.html" -type f -exec sed -i 's/embodiedcity.html/embodiedcity/g' {} +
